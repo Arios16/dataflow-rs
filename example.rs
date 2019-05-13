79de6156406be492bb8485c3ca24df9227350665
Andres Rios <agrios@uc.cl>
2019-05-13 08:16:13 -0400

    implemented analysis

--- example.rs
+++ example.rs
@@ -3,10 +3,28 @@ fn main() {
     println!("{}", y);
 }
 
-fn test_fn(x: i32) -> i32 {
-    if x < 0 {
+// fn test_fn(x: i32) -> i32 {
+//     if 0 < x {
+//         if x < 5 {
+//             return x;
+//         }
+//     }
+//     return 0;
+//     // if 0 < x && x < 5 {
+//     //     x
+//     // } else {
+//     //     0
+//     // }
+// }
+
+fn test_fn(mut x: i32) -> i32 {
+    // let y = 1+2;
+    // return y;
+    if x<0 {
         -x
     } else {
-        x
+        x+1
     }
 }
+
+fn black_box(x: i32) {}
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ extern crate rustc_driver;
 extern crate rustc_interface;
 
 mod block;
+mod lattice;
 
 use block::Block;
 use rustc::hir::def_id::LOCAL_CRATE;
-use rustc::mir::{BasicBlock, BasicBlockData, Mir, StatementKind, TerminatorKind, START_BLOCK};
+use rustc::mir::{
+    BasicBlock, BasicBlockData, Local, Mir, Operand, Place, PlaceBase, StatementKind,
+    TerminatorKind, START_BLOCK,
+};
+use rustc::ty::TyKind;
 use rustc_data_structures::indexed_vec::IndexVec;
 use rustc_interface::interface;
 use std::collections::{BinaryHeap, HashMap, HashSet};
@@ -34,17 +39,19 @@ fn reverse_post_order(
     *idx += 1;
 }
 
-struct Analysis<'tcx> {
+struct Analysis<'tcx, L: lattice::Lattice> {
     function_mir: &'tcx Mir<'tcx>,
+    input: IndexVec<BasicBlock, L>,
     order: HashMap<BasicBlock, usize>,
     worklist: BinaryHeap<Block<'tcx>>,
 }
 
-impl<'tcx> Analysis<'tcx> {
+impl<'tcx, L: lattice::Lattice> Analysis<'tcx, L> {
     fn new(function_mir: &'tcx Mir<'tcx>) -> Self {
         let mut order = HashMap::new();
         let mut idx = 0;
         let mut visited = HashSet::new();
+        let mut input = IndexVec::new();
         reverse_post_order(
             function_mir.basic_blocks(),
             START_BLOCK,
@@ -52,6 +59,11 @@ impl<'tcx> Analysis<'tcx> {
             &mut visited,
             &mut idx,
         );
+
+        for _ in 0..function_mir.basic_blocks().len() {
+            input.push(L::bot(&function_mir.local_decls));
+        }
+        input[START_BLOCK] = L::top(&function_mir.local_decls);
         let mut worklist = BinaryHeap::new();
         let first_block = Block::new(
             START_BLOCK,
@@ -63,20 +75,160 @@ impl<'tcx> Analysis<'tcx> {
             function_mir,
             order,
             worklist,
+            input,
+        }
+    }
+
+    fn run(&mut self) {
+        while let Some(block) = self.worklist.pop() {
+            let mut lattice = self.input[block.id].clone();
+            let mut lattice2 = None;
+            let mut if_local_bool = None;
+            let mut equivs = HashMap::new();
+
+            // To be able to propagate conditional information
+            match block.data.terminator().kind {
+                TerminatorKind::SwitchInt {
+                    ref discr,
+                    ref switch_ty,
+                    values: _,
+                    targets: _,
+                } => {
+                    if let TyKind::Bool = switch_ty.sty {
+                        match discr {
+                            Operand::Copy(place) | Operand::Move(place) => match place {
+                                Place::Base(PlaceBase::Local(local)) => {
+                                    if_local_bool = Some(local);
+                                }
+                                _ => {}
+                            },
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {}
+            }
+
+            // Process statements in this block
+            let mut finished = false;
+            for stmt in block.data.statements.iter() {
+                match stmt.kind {
+                    StatementKind::Assign(ref place, ref rvalue) => match place {
+                        Place::Base(place_base) => match place_base {
+                            PlaceBase::Local(local) => {
+                                if if_local_bool == Some(local) {
+                                    let r = lattice.flow_branch(rvalue, &mut equivs);
+                                    lattice = r.0;
+                                    lattice2 = Some(r.1);
+                                    finished = true;
+                                } else {
+                                    assert!(!finished);
+                                    lattice = lattice.flow_assign(*local, rvalue, &mut equivs);
+                                    println!("{:?}", lattice);
+                                }
+                            }
+                            _ => {}
+                        },
+                        _ => {}
+                    },
+                    _ => {} // We only really care about assignments
+                }
+            }
+
+            // Process function call if it exists
+            match block.data.terminator().kind {
+                TerminatorKind::Call {
+                    ref func,
+                    ref args,
+                    ref destination,
+                    cleanup: _,
+                    from_hir_call: _,
+                } => {
+                    if let Some((place, _)) = destination {
+                        lattice = lattice.flow_function_call(func, args, place);
+                    }
+                }
+                _ => {}
+            }
+
+            // Propagate information to block successors and add to worklist
+            let successors = block
+                .data
+                .terminator()
+                .successors()
+                .cloned()
+                .collect::<Vec<BasicBlock>>();
+            if if_local_bool.is_some(){
+                self.input[successors[0]] = lattice;
+                self.input[successors[1]] = lattice2.unwrap();
+                let b1 = Block::new(
+                    successors[0],
+                    &self.function_mir.basic_blocks()[successors[0]],
+                    self.order[&successors[0]],
+                );
+                let b2 = Block::new(
+                    successors[1],
+                    &self.function_mir.basic_blocks()[successors[1]],
+                    self.order[&successors[1]],
+                );
+                self.worklist.push(b1);
+                self.worklist.push(b2);
+            } else {
+                for suc in successors.into_iter(){
+                    self.input[suc] = lattice.clone();
+                    let b = Block::new(
+                        suc,
+                        &self.function_mir.basic_blocks()[suc],
+                        self.order[&suc]
+                    );
+                    self.worklist.push(b);
+                }
+            }
         }
     }
 
     fn print_mir(&self) {
         for (idx, block_data) in self.function_mir.basic_blocks().iter().enumerate() {
             let block_id = BasicBlock::from_usize(idx);
-            println!("block_id: {:?}", block_id);
+            println!("\t{} - block_id: {:?}", idx, block_id);
             for stmt in block_data.statements.iter() {
                 match stmt.kind {
-                    StatementKind::StorageDead(_) | StatementKind::StorageLive(_) => continue, // Not relevant for our purposes
-                    _ => println!("\t{:?}", stmt),
+                    // StatementKind::StorageDead(_) | StatementKind::StorageLive(_) => continue, // Not relevant for our purposes
+                    // _ => println!("\t\t{:?} {:?}", stmt, stmt.kind),
+                    StatementKind::Assign(ref place, ref rvalue) => {
+                        println!("\t\t place: {:?}, rvalue: {:?}", place, rvalue);
+                    }
+                    _ => {} // We only really care about assignments
                 }
             }
-            println!("\t{:?}", block_data.terminator());
+            println!("\t\t{:?}", block_data.terminator());
+            match block_data.terminator().kind {
+                TerminatorKind::Call {
+                    func: ref f,
+                    args: _,
+                    destination: ref d,
+                    cleanup: _,
+                    from_hir_call: _,
+                } => {
+                    println!("\t\tf: {:?}", f);
+                    println!("\t\td: {:?}", d);
+                }
+                TerminatorKind::Goto { target: block } => {
+                    println!("\t\tblock: {:?}", block);
+                }
+                TerminatorKind::SwitchInt {
+                    ref discr,
+                    switch_ty: ref ty,
+                    ref values,
+                    ref targets,
+                } => {
+                    println!(
+                        "\t\tdiscr: {:?}, switch_ty: {:?}, values: {:?}, targets: {:?}",
+                        discr, ty, values, targets
+                    );
+                }
+                _ => {}
+            }
         }
     }
 }
@@ -90,11 +242,34 @@ impl rustc_driver::Callbacks for CompilerCallback {
             let set = tcx.mir_keys(LOCAL_CRATE);
             for &key in set.iter() {
                 let module_name = key.describe_as_module(tcx);
-                let (start,end) = (module_name.find('`').unwrap()+1, module_name.rfind('`').unwrap());
-                let fn_name = module_name.chars().skip(start).take(end-start).collect::<String>();
+                let (start, end) = (
+                    module_name.find('`').unwrap() + 1,
+                    module_name.rfind('`').unwrap(),
+                );
+                let fn_name = module_name
+                    .chars()
+                    .skip(start)
+                    .take(end - start)
+                    .collect::<String>();
+                if fn_name != "test_fn" {
+                    continue;
+                }
                 println!("function: {}", fn_name);
+                println!("function: {}", module_name);
                 let mir = tcx.optimized_mir(key);
-                let analysis = Analysis::new(mir);
+                // for (local, decl) in mir.local_decls.iter_enumerated() {
+                //     println!("{:?} {:?}", local, decl.ty);
+                //     match decl.ty.sty {
+                //         TyKind::Int(a) => {
+                //             println!("{:?}", a);
+                //         }
+                //         _ => {}
+                //     }
+                // }
+                let mut analysis =
+                    Analysis::<HashMap<Local, lattice::SignAnalysisSimpleLattice>>::new(mir);
+                analysis.run();
+                println!("{:?}", analysis.input);
                 analysis.print_mir();
                 println!();
             }
